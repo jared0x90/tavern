9f95ec8dd5d3e6c0d4b6aa11c0e5d26d6fd45f07
Jared De Blander <jared@deblander.org>
2013-09-19 13:10:53 -0400

    Domain parser

--- tavern.py
+++ tavern.py
@@ -1,4 +1,4 @@
-import flask
+from flask import *
 import language
 import settings
 
@@ -12,3 +12,23 @@ app = Flask(
     static_folder='static',
     static_url_path='/static'
 )
+
+# before requests are processed
+@app.before_request
+def before_request():
+    # split the request URL into peices and grab the domain and path
+    url_base = str(request.url)
+    url_parts = url_base.split('/', 3)
+    g.request_domain = url_parts[2]
+    g.request_path = url_parts[3]
+
+
+# create routes
+@app.route("/")
+def index():
+    return g.request_domain
+
+# start flask
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 80)
